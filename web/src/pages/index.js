1e7102ae09aa2426b66ec7ac6718cab9ea8fe299
Mirek Ostafinski <ostafinskim@gmail.com>
2021-04-15 20:58:13 +0100

    fix: main section added

--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -1,12 +1,7 @@
 import React from "react";
-import ContactForm from "../components/ContactForm";
 import styled from "styled-components";
-const IndexContainer = styled.div`
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
+
+const MainStyles = styled.main`
   h1 {
     font-size: min(max(3.75rem, 3vw), 7rem);
     @supports (font-size: clamp(3.75rem, 3vw, 7rem)) {
@@ -20,13 +15,10 @@ const IndexContainer = styled.div`
 
 const IndexPage = () => {
   return (
-    <>
-      <IndexContainer>
-        <h1>Mirek Ostafinski</h1>
-        <p>front-end developer</p>
-        <ContactForm />
-      </IndexContainer>
-    </>
+    <MainStyles>
+      <h1>Mirek Ostafinski</h1>
+      <p>front-end developer</p>
+    </MainStyles>
   );
 };
 
