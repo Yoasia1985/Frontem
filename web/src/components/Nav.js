b6d5d694a858c2a5e6d1406d17e417b037697540
Mirek Ostafinski <ostafinskim@gmail.com>
2021-04-16 22:43:22 +0100

    feat: Nav styles

--- web/src/components/Nav.js
+++ web/src/components/Nav.js
@@ -3,14 +3,30 @@ import { Link } from "gatsby";
 import styled from "styled-components";
 
 const NavStyles = styled.nav`
-  width: 100%;
+  height: 9rem;
   ul {
+    height: 100%;
     display: flex;
-    justify-content: flex-end;
+    justify-content: center;
+    align-items: center;
     list-style: none;
   }
   li {
     margin-right: 3rem;
+    transform: rotate(-3deg);
+    transition: all 0.3s ease;
+    border-bottom: 3px solid transparent;
+    &:hover {
+      transform: rotate(3deg);
+      border-bottom: 3px solid var(--yellow);
+    }
+    a {
+      font-family: "Indigo Regular";
+      font-size: 2.5rem;
+      text-decoration: none;
+      text-transform: uppercase;
+      color: var(--violet);
+    }
   }
 `;
 export default function Nav() {
