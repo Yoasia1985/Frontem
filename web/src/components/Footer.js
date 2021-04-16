b64f1e8795c9256e657d4b6c3873d3160ad9864d
Mirek Ostafinski <ostafinskim@gmail.com>
2021-04-16 06:56:08 +0100

    contact moved once again

--- web/src/components/Footer.js
+++ web/src/components/Footer.js
@@ -1,10 +1,8 @@
 import React from "react";
-import ContactForm from "./ContactForm";
 
 export default function Footer() {
   return (
     <footer>
-      <ContactForm />
       <p>Frontem &copy; {new Date().getFullYear()}</p>
     </footer>
   );
