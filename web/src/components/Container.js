0ca7d08d2ef06b2a8a896c98aa0ed1293bde8249
Mirek Ostafinski <ostafinskim@gmail.com>
2021-04-16 22:41:50 +0100

    fix: main grid container size changed

--- web/src/components/Container.js
+++ web/src/components/Container.js
@@ -5,14 +5,22 @@ const ContainerStyles = styled.div`
   margin: 0 auto;
   width: 100%;
   display: grid;
-  grid-template-columns: 1fr min(60ch, calc(100% - 64px)) 1fr;
-  grid-template-rows: max-content 1fr max-content;
-  grid-column-gap: 32px;
+  grid-template-columns: 1fr min(90ch, calc(100% - 64px)) 1fr;
+  grid-auto-rows: auto;
+  grid-gap: 32px;
   & > * {
     grid-column: 2;
-    background: var(--white);
     box-shadow: var(--boxShadow);
+    background: rgb(239, 237, 243);
     padding: 2rem 3rem;
+  }
+  & :first-child {
+    grid-column: 1 / -1;
+    padding: 0;
+    margin: 0;
+  }
+  & :last-child {
+    grid-column: 1 / -1;
   }
 `;
 export default function Container({ children }) {
