23e6710d7d3b8e23bdd458023a42862e66b5bd51
Mirek Ostafinski <ostafinskim@gmail.com>
2021-04-16 06:57:41 +0100

    feat: couple colors added

--- web/src/styles/GlobalStyles.js
+++ web/src/styles/GlobalStyles.js
@@ -7,10 +7,17 @@ const GlobalStyles = createGlobalStyle`
     --black: #2E2E2E;
     --yellow: #ffc600;
     --white: #fff;
-    --grey: #efefef;
+    --grey: #f6f9fb;
+    --grey-secondary: #dde4e9;
+    --grey-darker: #474f5b;
+    --boxShadow: 0 3px 8px 0 rgb(0 0 0 / 3%);
+  }
+  html {
+    box-sizing: border-box;
   }
   body {
     font-size: 2rem;
+    background: var(--grey);
   }
 `;
 
