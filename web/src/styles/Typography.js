bc8b9b1f91d59663f680984ad7d5e80beb9a0e35
Mirek Ostafinski <ostafinskim@gmail.com>
2021-04-18 18:30:11 +0100

    feat: typography styles

--- web/src/styles/Typography.js
+++ web/src/styles/Typography.js
@@ -1,27 +1,20 @@
 import { createGlobalStyle } from "styled-components";
-
-import indigR from "../assets/fonts/indigo_regular.woff2";
-import indigO from "../assets/fonts/indigo_outline.woff2";
+import Merriweather from "../assets/fonts/merriweather-regular-webfont.woff2";
+import Montserrat from "../assets/fonts/montserrat-bold-webfont.woff2";
 
 const Typography = createGlobalStyle`
 @font-face {
-    font-family: 'Indigo Outline';
-    src: url(${indigO});
+    font-family: 'Merriweather';
+    src: url(${Merriweather});
     font-weight: normal;
     font-style: normal;
-
 }
 @font-face {
-    font-family: 'Indigo Regular';
-    src: url(${indigR});
+    font-family: 'Montserrat';
+    src: url(${Montserrat});
     font-weight: normal;
     font-style: normal;
 }
-  html {
-    font-size: 10px;
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-    line-height: 1.5;
-  }
   p, li {
     letter-spacing: 0.5px;
   }
