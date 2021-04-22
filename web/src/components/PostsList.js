738eb7cc9ffea7c666c55ed6d50b677efce6cfc8
Mirek Ostafinski <ostafinskim@gmail.com>
2021-04-22 20:14:44 +0100

    feat: Post component now PostPreview component

--- web/src/components/PostsList.js
+++ web/src/components/PostsList.js
@@ -1,22 +1,13 @@
 import React from "react";
-import Post from "./Post";
-import styled from "styled-components";
-
-const PostListStyles = styled.div`
-  margin: 0 1rem;
-  display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(500px, 1fr));
-  gap: 4rem;
-  grid-auto-rows: max-content 500px;
-`;
+import PostPreview from "./PostPreview";
 
 const PostsList = ({ data }) => {
   return (
-    <PostListStyles>
+    <>
       {data.allPosts.nodes.map((post) => (
-        <Post key={post.id} data={post} />
+        <PostPreview key={post.id} data={post} />
       ))}
-    </PostListStyles>
+    </>
   );
 };
 
