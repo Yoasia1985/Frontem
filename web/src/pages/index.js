eadc7a90b54576987a6a3e36b0786884a41afca1
Mirek Ostafinski <ostafinskim@gmail.com>
2021-04-22 20:17:52 +0100

    feat: index page styles

--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -9,9 +9,33 @@ const MainStyles = styled.main`
   display: grid;
   grid-template-columns: 1fr;
   grid-template-rows: max-content 1fr;
+  justify-items: center;
   .hero {
-    display: flex;
-    justify-content: space-around;
+    padding: 2.5rem 0;
+    width: 100%;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-row-gap: 2.5rem;
+    grid-template-rows: repeat(2, max-content);
+    h1 {
+      grid-column: 1 / -1;
+      grid-row: 2 / 3;
+      justify-self: center;
+      font-size: min(max(3.75rem, 3vw), 9rem);
+      @supports (font-size: clamp(3.75rem, 3vw, 9rem)) {
+        font-size: clamp(3.75rem, 3vw, 9rem);
+      }
+      font-family: "Merriweather", serif;
+      letter-spacing: 3px;
+      color: var(--color-bg);
+      span {
+        color: var(--black);
+        font-size: 1.6rem;
+        display: block;
+        font-family: monospace;
+        text-align: center;
+      }
+    }
     @media (max-width: 485px) {
       flex-direction: column-reverse;
       h1 {
@@ -19,28 +43,14 @@ const MainStyles = styled.main`
       }
     }
   }
-  p {
-    text-align: center;
-  }
-  h1 {
-    font-size: min(max(3.75rem, 3vw), 9rem);
-    @supports (font-size: clamp(3.75rem, 3vw, 9rem)) {
-      font-size: clamp(3.75rem, 3vw, 9rem);
-    }
-    font-family: "Merriweather", serif;
-    letter-spacing: 3px;
-    color: var(--color-bg);
-    span {
-      color: var(--black);
-      font-size: 1.6rem;
-      display: block;
-      font-family: monospace;
-    }
-  }
+
   .pic {
-    align-self: center;
-    width: 200px;
-    height: 200px;
+    width: 100%;
+    grid-column: 1 / -1;
+    grid-row: 1 / 2;
+    justify-self: center;
+    width: 300px;
+    height: 300px;
     border: double 6px transparent;
     box-shadow: var(--boxShadow);
     border-radius: 50%;
@@ -177,15 +187,13 @@ const IndexPage = () => {
     <>
       <MainStyles>
         <section className="hero">
-          <h1>
-            Hi!
-            <br />
-            I&#39;m Miro.
-            <span>front-end developer</span>
-          </h1>
           <div className="pic bounce-in-top">
             {pic ? <img src={pic} alt="Me" /> : ""}
           </div>
+          <h1>
+            Hi! I&#39;m Miro.
+            <span>front-end developer</span>
+          </h1>
         </section>
         <Stack />
       </MainStyles>
