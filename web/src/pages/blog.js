b5705a5515b6035081822901b13cc1591a7227b5
Mirek Ostafinski <ostafinskim@gmail.com>
2021-04-22 06:55:05 +0100

    feat: blog page query all posts from Sanity and render PostsList component passing data

--- web/src/pages/blog.js
+++ web/src/pages/blog.js
@@ -1,9 +1,54 @@
 import React from "react";
+import { graphql } from "gatsby";
+import PostsList from "../components/PostsList";
+import styled from "styled-components";
 
-export default function BlogPage() {
+const BlogPageStyles = styled.main`
+  grid-column: 1 / -1;
+`;
+export const query = graphql`
+  query MyQuery {
+    allPosts: allSanityPost {
+      nodes {
+        id
+        slug {
+          current
+        }
+        mainImage {
+          asset {
+            fluid(maxWidth: 400) {
+              ...GatsbySanityImageFluid
+            }
+          }
+          caption
+        }
+        authors {
+          author {
+            name
+            image {
+              asset {
+                fluid(maxWidth: 150) {
+                  ...GatsbySanityImageFluid
+                }
+              }
+            }
+          }
+        }
+        title
+        publishedAt(formatString: "DD-MM-YYYY")
+        body {
+          children {
+            text
+          }
+        }
+      }
+    }
+  }
+`;
+export default function BlogPage({ data }) {
   return (
-    <>
-      <p>Hi I am BlogPage</p>
-    </>
+    <BlogPageStyles>
+      <PostsList data={data}></PostsList>
+    </BlogPageStyles>
   );
 }
