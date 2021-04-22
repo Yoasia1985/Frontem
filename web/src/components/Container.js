06aa1a3eac9aae5996030ec83b008bcc6a81e0e4
Mirek Ostafinski <ostafinskim@gmail.com>
2021-04-22 20:11:21 +0100

    feat: container styles

--- web/src/components/Container.js
+++ web/src/components/Container.js
@@ -2,18 +2,15 @@ import React from "react";
 import styled from "styled-components";
 
 const ContainerStyles = styled.div`
-  height: 100vh;
   margin: 0 auto;
-  background: #efede7;
   display: grid;
   grid-template-columns: 1fr minmax(29rem, 60ch) 1fr;
   & > * {
     grid-column: 2;
   }
   header {
-    height: 9rem;
     padding: 0;
-    margin: 2rem 0;
+    margin: 2.5rem 0 1.5rem 0;
   }
 `;
 export default function Container({ children }) {
