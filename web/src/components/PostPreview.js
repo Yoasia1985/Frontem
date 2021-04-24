9be9dc05950e69a606145c295524a7c8c29fc46d
Mirek Ostafinski <ostafinskim@gmail.com>
2021-04-24 07:23:39 +0100

    feat: postList and postPreview

--- web/src/components/PostPreview.js
+++ web/src/components/PostPreview.js
@@ -1,15 +1,48 @@
 import React from "react";
 import { Link } from "gatsby";
-import Img from "gatsby-image";
+import styled from "styled-components";
 
-const PostPreview = () => {
+const PreviewStyles = styled.div`
+  display: flex;
+  justify-content: center;
+  padding: 1.6rem;
+  h2 {
+    margin-bottom: 1rem;
+  }
+  a {
+    background: #eef2ff;
+    padding: 1.5rem 1.5rem 1.5rem 3rem;
+    margin-bottom: 2rem;
+    border-radius: 3px;
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    text-decoration: none;
+    color: var(--color-form);
+    box-shadow: var(--boxShadow);
+  }
+  .img {
+    width: 30rem;
+    height: 22rem;
+    border-radius: 6px;
+    img {
+      border-radius: 6px;
+      width: 100%;
+      height: 100%;
+    }
+  }
+`;
+const PostPreview = ({ data }) => {
   return (
-    <>
-      <Link>
-        <h2>sometext</h2>
+    <PreviewStyles>
+      <Link to={`/blog/${data.slug.current}`}>
+        <h2>{data.title}</h2>
+        <div className="img">
+          <img src={data.mainImage.asset.fluid.src} alt={data.title} />
+        </div>
       </Link>
-      <Img />
-    </>
+    </PreviewStyles>
   );
 };
 
--- web/src/components/PostsList.js
+++ web/src/components/PostsList.js
@@ -1,7 +1,17 @@
 import React from "react";
-
-const PostsList = () => {
-  return <></>;
+import PostPreview from "./PostPreview";
+import styled from "styled-components";
+const ListStyles = styled.div`
+  min-height: 100vh;
+`;
+const PostsList = ({ data }) => {
+  return (
+    <ListStyles>
+      {data.posts.nodes.map((post) => {
+        return <PostPreview key={post.id} data={post} />;
+      })}
+    </ListStyles>
+  );
 };
 
 export default PostsList;
